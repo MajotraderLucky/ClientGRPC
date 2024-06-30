319756ac3c49466a98e9a68c488a3de8f30957ea
Sergey Ryazanov <majo78@mail.ru>
2024-06-30 05:17:26 +0300

    refactor(main): Refactor main function to enhance modularity and readability
    This commit refactors the main function to improve the modularity and readability of the code. By isolating specific functionalities into separate functions, the codebase becomes cleaner and easier to maintain.
    
    Changes:
    - Created `loadConfig` function to encapsulate the configuration loading process. This function is responsible for reading the configuration file and handling any errors related to this operation.
    - Created `runGRPCClient` function that handles all interactions with the gRPC server. This function establishes the gRPC connection, makes the echo request, and processes the response.
    - Updated `main` to call these new functions, simplifying its logic and focusing only on the initial sequence of execution steps.
    
    These changes not only make the `main` function cleaner and more understandable but also enhance the overall structure of the application by defining clear responsibilities for each part of the code.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,15 +5,26 @@ import (
 	"clientgrpc/internal/grpcclient"
 	"log"
 
-	pb "github.com/MajotraderLucky/ServerGRPC/api/proto/pb"
+	"github.com/MajotraderLucky/ServerGRPC/api/proto/pb"
 )
 
 func main() {
-	cfg, err := config.LoadConfig("config/config.json")
+	cfg, err := loadConfig("config/config.json")
 	if err != nil {
-		log.Fatalf("could not load config: %v", err)
+		log.Fatalf("Error loading config: %v", err)
 	}
+	runGRPCClient(cfg)
+}
 
+func loadConfig(path string) (*config.Config, error) {
+	cfg, err := config.LoadConfig(path)
+	if err != nil {
+		return nil, err // Обработка ошибки перенесена в main
+	}
+	return cfg, nil
+}
+
+func runGRPCClient(cfg *config.Config) {
 	conn, err := grpcclient.CreateGRPCConnection(cfg)
 	if err != nil {
 		log.Fatalf("could not connect to server: %v", err)
@@ -21,7 +32,6 @@ func main() {
 	defer conn.Close()
 
 	c := pb.NewSimpleServiceClient(conn)
-
 	response, err := grpcclient.MakeEchoRequest(c, "Hello, server!")
 	if err != nil {
 		log.Fatalf("could not make echo request: %v", err)
