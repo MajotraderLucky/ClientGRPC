5e6db974068ba1cd8b91b50558ae99e029a1f2c8
Sergey Ryazanov <majo78@mail.ru>
2024-06-30 05:31:02 +0300

    refactor(main): Utilize existing LoadConfig function in main application flow
    This commit updates the main.go to directly use the existing LoadConfig function from the internal/config package. The change ensures that the application adheres to DRY principles by utilizing the already defined functionality for loading configuration settings.
    
    Changes:
    - Updated main.go to call config.LoadConfig directly with the path to the configuration file.
    - Removed any duplicate LoadConfig function definitions (if any were previously created) to eliminate redundancy.
    
    This modification simplifies the codebase and ensures that all parts of the application leverage the centralized configuration loading mechanism, enhancing maintainability and consistency across the project.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,18 +7,10 @@ import (
 )
 
 func main() {
-	cfg, err := loadConfig("config/config.json")
+	cfg, err := config.LoadConfig("config/config.json")
 	if err != nil {
 		log.Fatalf("Error loading config: %v", err)
 	}
 	clientService := grpcclient.NewGRPCClientService(cfg)
 	clientService.RunGRPCClient()
-}
-
-func loadConfig(path string) (*config.Config, error) {
-	cfg, err := config.LoadConfig(path)
-	if err != nil {
-		return nil, err // Обработка ошибки перенесена в main
-	}
-	return cfg, nil
 }
